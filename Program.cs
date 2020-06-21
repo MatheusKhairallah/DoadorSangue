028504830d267faeb565b50c7b238913100f91ab
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-20 21:44:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua idade: ");
+            double idade=Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            if(idade>18 && idade<67)
+            Console.WriteLine("Você pode ser doador de sangue");
+            else
+            Console.WriteLine("Você não pode ser doador de sangue");
+        }
+    }
+}
